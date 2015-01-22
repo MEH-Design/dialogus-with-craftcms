cc2e28e2c3019ece0c0a42c0190b30a90b382e72
Christoph Minixhofer <minchc11@htlkaindorf.at>
2015-01-22 20:25:34 +0100

    styleguide hotfix

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -19,118 +19,120 @@ module.exports = function(grunt) {
               return done(
                 new Error('r.js built duplicate modules,' +
                 'please check the excludes option.'));
-            } else {
-              grunt.log.success('No duplicates found!');
-            }
+              } else {
+                grunt.log.success('No duplicates found!');
+              }
 
-            done();
+              done();
+            }
           }
         }
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'app/js/*.js']
-    },
-    jscs: {
-      app: 'app/js/*.js',
-      gruntfile: 'Gruntfile.js',
-      options: {
-        preset: 'google'
-      }
-    },
-    scsslint: {
-      allFiles: ['app/sass/**/*.scss'],
-      options: {
-        config: '.scss-lint.yml'
-      }
-    },
-    sass: {
-      dist: {
+      },
+      jshint: {
+        all: ['Gruntfile.js', 'app/js/*.js']
+      },
+      jscs: {
+        app: 'app/js/*.js',
+        gruntfile: 'Gruntfile.js',
         options: {
-          style: 'expanded',
-          require: 'susy'
-        },
-        files: {
-          'app/css/main.css': 'app/sass/main.scss'
+          preset: 'google'
         }
-      }
-    },
-    sassdown: {
-      defaultStyleguide: {
+      },
+      scsslint: {
+        allFiles: ['app/sass/**/*.scss'],
         options: {
-          assets: [
+          config: '.scss-lint.yml'
+        }
+      },
+      sass: {
+        dist: {
+          options: {
+            style: 'expanded',
+            require: 'susy'
+          },
+          files: {
+            'app/css/main.css': 'app/sass/main.scss'
+          }
+        }
+      },
+      sassdown: {
+        defaultStyleguide: {
+          options: {
+            assets: [
             'app/css/*.css'
-          ],
-          scripts: [
+            ],
+            scripts: [
             'node_modules/sassdown/tasks/data/scripts.js',
             'http://craft.dev:35729/livereload.js'
-          ],
-          highlight: 'github',
-          excludeMissing: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'app/sass',
-          src: ['*.scss'],
-          dest: 'styleguide/'
-        }]
-      }
-    },
-    autoprefixer: {
-
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9']
-      },
-
-      all: {
-        flatten: true,
-        src: 'app/css/*.css',
-        dest: 'app/css/main.css'
-      }
-    },
-    watch: {
-
-      styles: {
-        files: ['app/sass/*.scss'],
-        tasks: ['styles'],
-        options: {
-          livereload: true
+            ],
+            highlight: 'github',
+            excludeMissing: true
+          },
+          files: [{
+            expand: true,
+            cwd: 'app/sass',
+            src: [
+            '**/*.scss'
+            ],
+            dest: 'styleguide/'
+          }]
         }
-      }
-    },
-  });
+      },
+      autoprefixer: {
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-scss-lint');
-  grunt.loadNpmTasks('grunt-jscs');
-  grunt.loadNpmTasks('sassdown');
+        options: {
+          browsers: ['last 2 versions', 'ie 8', 'ie 9']
+        },
 
-  grunt.registerTask('default', [
+        all: {
+          flatten: true,
+          src: 'app/css/*.css',
+          dest: 'app/css/main.css'
+        }
+      },
+      watch: {
+        styles: {
+          files: ['app/sass/**/*.scss'],
+          tasks: ['styles'],
+          options: {
+            livereload: true
+          }
+        }
+      },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-scss-lint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('sassdown');
+
+    grunt.registerTask('default', [
     'scsslint',
     'jscs',
     'jshint',
     'sassdown',
     'sass',
     'autoprefixer'
-  ]);
+    ]);
 
-  grunt.registerTask('styles', [
+    grunt.registerTask('styles', [
     'sassdown',
     'sass',
     'autoprefixer'
-  ]);
+    ]);
 
-  grunt.registerTask('test', [
+    grunt.registerTask('test', [
     'jscs',
     'jshint',
     'scsslint'
-  ]);
+    ]);
 
-  grunt.registerTask('server', [
+    grunt.registerTask('server', [
     'watch'
-  ]);
-};
+    ]);
+  };
+  
