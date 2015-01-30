86093d0a03d79e17bd1491a39604e98c36df34bc
Christoph Minixhofer <minchc11@htlkaindorf.at>
2015-01-30 20:54:49 +0100

    fix property sort order

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          style: 'expanded',
+          style: 'compressed',
           require: 'susy'
         },
         files: {
