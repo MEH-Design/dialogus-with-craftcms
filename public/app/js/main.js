840bbdae928a6f3aef3ada5c7df57d5bbb4f4bd0
jakiii7 <eisjac11@new.edvhtl.at>
2015-01-31 23:14:47 +0100

    fixed mobile header animation

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,6 +1,16 @@
 requirejs(['config'], function() {
   requirejs(['modernizr']);
   requirejs(['gsap'], function() {
-    
+    document.getElementsByClassName("header__hamburger")[0].onclick=function(){
+      var elem = document.getElementsByClassName("header")[0];
+      if(elem.classList.contains("expanded")){
+        elem.classList.remove("expanded");
+        TweenMax.to(elem, 1, {height:'22vw'});
+      }
+      else{
+        elem.classList.add("expanded");
+        TweenMax.to(elem, 1, {height:'70vw'});
+      }
+    }
   });
 });
