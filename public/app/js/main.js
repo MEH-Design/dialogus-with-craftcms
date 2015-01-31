24d0878c9e3dbf8a459379dc48e604e5796e8233
jakiii7 <eisjac11@new.edvhtl.at>
2015-01-31 22:04:00 +0100

    add header expansion on button click

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,6 +1,12 @@
 requirejs(['config'], function() {
   requirejs(['modernizr']);
   requirejs(['gsap'], function() {
-  
+
   });
 });
+
+function buttonClick()
+{
+  var menu = document.getElementById("header__menu");
+  TweenLight.to(menu, 1.5, {height:200});
+}
