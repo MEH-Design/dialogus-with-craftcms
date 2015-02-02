47ff747ca9f59a17aa5a57f9ae6e27d1d5f4906b
Christoph Minixhofer <minchc11@htlkaindorf.at>
2015-02-02 11:13:26 +0100

    fixed jscs errors

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,21 +1,22 @@
 requirejs(['config'], function() {
   requirejs(['modernizr']);
   requirejs(['gsap'], function() {
-    document.getElementsByClassName("header__hamburger")[0].onclick=function(){
-      var header = document.getElementsByClassName("header")[0];
-      var nav = document.getElementsByClassName("header__menu")[0];
-      if(header.classList.contains("header--expanded")){
-        header.classList.remove("header--expanded");
+    document.getElementsByClassName('header__hamburger')[0].onclick =
+    function() {
+      var header = document.getElementsByClassName('header')[0];
+      var nav = document.getElementsByClassName('header__menu')[0];
+      if (header.classList.contains('header--expanded')) {
+        header.classList.remove('header--expanded');
         TweenMax.to(header, 1, {height:'22vw'});
-      }
-      else{
-        header.classList.add("header--expanded");
+      } else {
+        header.classList.add('header--expanded');
         TweenMax.to(header, 1, {height:'80vw'});
       }
-    }
+    };
 
     function VWtoPX(vw) {
-      return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth / 100 * vw;
+      return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth /
+      100 * vw;
     }
   });
 });
