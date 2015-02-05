8088ed65ef36a5de6c1c0cae95747786e6a9d46e
jakiii7 <eisjac11@new.edvhtl.at>
2015-02-05 16:10:41 +0100

    improved header animation added closing on press feature

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -20,17 +20,39 @@ require(['clipPath', 'browserdetect'], function(clipPath) {
   }
 });
 require(['TweenLite', 'EasePack', 'CSSPlugin'], function(TweenLite) {
+  var header = document.getElementsByClassName('header')[0];
   var expanded = false;
+  var div = document.createElement('div');
+  var body = document.body;
+  div.style.opacity = 0;
+  div.style.position= 'fixed';
+  div.style.top= 0;
+  div.style.left= 0;
+  div.style.backgroundColor = 'black';
+  div.style.width= '100%';
+  div.style.height= '150%';
+  div.style.zIndex = 10;
   document.getElementsByClassName('header__hamburger')[0].onclick =
   function() {
-    var header = document.getElementsByClassName('header')[0];
+    var nav = document.getElementsByClassName('header__menu')[0];
+    var ul = nav.getElementsByTagName('ul')[0];
+    var height = VWtoPX(24) + ul.offsetHeight;
     if (expanded) {
-      TweenLite.to(header, 1, {height:'22vw'});
+      TweenLite.to(header, .5, {height:'22vw', ease:Circ.easeInOut});
+      TweenLite.to(div, .5, {opacity:0, onComplete:function() {body.removeChild(div);}});
     } else {
-      TweenLite.to(header, 1, {height:'80vw'});
+      TweenLite.to(header, .5, {height:height, ease:Circ.easeInOut});
+      TweenLite.to(div, .5, {opacity:0.6});
+      body.appendChild(div);
     }
     expanded=!expanded;
   };
+
+  div.onclick = function() {
+    TweenLite.to(header, .5, {height:'22vw', ease:Circ.easeInOut});
+    TweenLite.to(div, .5, {opacity:0, onComplete:function() {body.removeChild(div);}});
+    expanded=false;
+  }
 
   function VWtoPX(vw) {
     return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth /
