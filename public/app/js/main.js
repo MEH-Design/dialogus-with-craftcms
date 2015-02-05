e17d430c17f06be76f2966f3a9236516f98cb84b
Christoph Minixhofer <minchc11@htlkaindorf.at>
2015-02-05 20:22:04 +0100

    cleaned up js code

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -10,52 +10,20 @@ require.config({
 });
 
 require(['modernizr']);
-require(['clipPath', 'browserdetect'], function(clipPath) {
-  if (BrowserDetect.browser != 'Chrome') {
-    var clip = new clipPath();
+
+require(['ClipPath', 'browserdetect'], function(ClipPath) {
+  'use strict';
+  if (BrowserDetect.browser != 'Google') {
+    var clip = new ClipPath();
     clip.register();
     window.onresize = function() {
       clip.rerun();
     };
   }
 });
-require(['TweenLite', 'EasePack', 'CSSPlugin'], function(TweenLite) {
-  var header = document.getElementsByClassName('header')[0];
-  var expanded = false;
-  var div = document.createElement('div');
-  var body = document.body;
-  div.style.opacity = 0;
-  div.style.position= 'fixed';
-  div.style.top= 0;
-  div.style.left= 0;
-  div.style.backgroundColor = 'black';
-  div.style.width= '100%';
-  div.style.height= '150%';
-  div.style.zIndex = 10;
-  document.getElementsByClassName('header__hamburger')[0].onclick =
-  function() {
-    var nav = document.getElementsByClassName('header__menu')[0];
-    var ul = nav.getElementsByTagName('ul')[0];
-    var height = VWtoPX(24) + ul.offsetHeight;
-    if (expanded) {
-      TweenLite.to(header, .5, {height:'22vw', ease:Circ.easeInOut});
-      TweenLite.to(div, .5, {opacity:0, onComplete:function() {body.removeChild(div);}});
-    } else {
-      TweenLite.to(header, .5, {height:height, ease:Circ.easeInOut});
-      TweenLite.to(div, .5, {opacity:0.6});
-      body.appendChild(div);
-    }
-    expanded=!expanded;
-  };
 
-  div.onclick = function() {
-    TweenLite.to(header, .5, {height:'22vw', ease:Circ.easeInOut});
-    TweenLite.to(div, .5, {opacity:0, onComplete:function() {body.removeChild(div);}});
-    expanded=false;
-  }
-
-  function VWtoPX(vw) {
-    return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth /
-    100 * vw;
-  }
+require(['HeaderAnimation'], function(HeaderAnimation) {
+  'use strict';
+  var header = new HeaderAnimation();
+  header.init();
 });
