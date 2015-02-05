1bc2b2ea0044709c9c5e94f49212b3071fd77f14
jakiii7 <eisjac11@new.edvhtl.at>
2015-02-05 15:15:35 +0100

    fixed tweenLite issues

--- public/app/js/config.js
+++ public/app/js/config.js
@@ -1,14 +1,1 @@
-requirejs.config({
-   paths: {
-     gsap: '/app/bower_components/gsap/src/uncompressed/TweenLite',
-     modernizr: '/app/bower_components/modernizr/modernizr',
-     polyfill: '/app/bower_components/polyfill.js/dist/polyfill',
-     browserdetect: '/app/bower_components/browserdetect/browserdetect'
-   },
-   shim: {
-    'libs/gsap/TweenLite': {
-        deps:    ['/app/bower_components/gsap/src/uncompressed/TweenLite'],
-        exports: 'TweenLite'
-    }
-   }
-});
+
--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,32 +1,39 @@
-require(['config'], function() {
-  require(['modernizr']);
-  require(['clipPath', 'browserdetect'], function(clipPath) {
-    if (BrowserDetect.browser != 'Chrome') {
-      var clip = new clipPath();
-      clip.register();
-      window.onresize = function() {
-        clip.rerun();
-      };
-    }
-  });
-  require(['gsap'], function() {
-    document.getElementsByClassName('header__hamburger')[0].onclick =
-    function() {
-      var header = document.getElementsByClassName('header')[0];
-      var nav = document.getElementsByClassName('header__menu')[0];
-      if (header.classList.contains('header--expanded')) {
-        header.classList.remove('header--expanded');
-        var tween = TweenLite.to(header, 1, {height:'22vw'});
-        console.log(tween);
-      } else {
-        header.classList.add('header--expanded');
-        TweenLite(header, 1, {height:'80vw'});
-      }
-    };
+require.config({
+   paths: {
+     TweenLite: '/app/bower_components/gsap/src/uncompressed/TweenLite',
+     CSSPlugin: '/app/bower_components/gsap/src/uncompressed/plugins/CSSPlugin',
+     EasePack: '/app/bower_components/gsap/src/uncompressed/easing/EasePack',
+     modernizr: '/app/bower_components/modernizr/modernizr',
+     polyfill: '/app/bower_components/polyfill.js/dist/polyfill',
+     browserdetect: '/app/bower_components/browserdetect/browserdetect'
+   }
+});
 
-    function VWtoPX(vw) {
-      return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth /
-      100 * vw;
+require(['modernizr']);
+require(['clipPath', 'browserdetect'], function(clipPath) {
+  if (BrowserDetect.browser != 'Chrome') {
+    var clip = new clipPath();
+    clip.register();
+    window.onresize = function() {
+      clip.rerun();
+    };
+  }
+});
+require(['TweenLite', 'EasePack', 'CSSPlugin'], function(TweenLite) {
+  var expanded = false;
+  document.getElementsByClassName('header__hamburger')[0].onclick =
+  function() {
+    var header = document.getElementsByClassName('header')[0];
+    if (expanded) {
+      TweenLite.to(header, 1, {height:'22vw'});
+    } else {
+      TweenLite.to(header, 1, {height:'80vw'});
     }
-  });
+    expanded=!expanded;
+  };
+
+  function VWtoPX(vw) {
+    return isNaN(window.innerWidth) ? window.clientWidth : window.innerWidth /
+    100 * vw;
+  }
 });
