d6c8525de163a39729fcf0aa2b3f86e8ae11897d
Christoph Minixhofer <minchc11@htlkaindorf.at>
2015-02-05 21:22:53 +0100

    add safari to polyfill exceptions

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,7 +1,7 @@
 require(['config'], function() {
   require(['modernizr']);
   require(['clipPath', 'browserdetect'], function(clipPath) {
-    if (BrowserDetect.browser != 'Chrome') {
+    if (!(BrowserDetect.browser == 'Chrome' || BrowserDetect.browser == 'Safari')) {
       var clip = new clipPath();
       clip.register();
       window.onresize = function() {
