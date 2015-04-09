2a80ce246f1f630f3a1d7bbc8b292acb0c0f5ccc
1D0BE <matthias.hofreiter@gmail.com>
2015-04-09 14:21:31 +0200

    Letzter Commit

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -6,18 +6,18 @@ if (!supports) {
 }
 
 require.config({
-   paths: {
-     TweenLite:
-     '/app/bower_components/gsap/src/uncompressed/TweenLite',
-     CSSPlugin:
-     '/app/bower_components/gsap/src/uncompressed/plugins/CSSPlugin',
-     CSSRulePlugin:
-     '/app/bower_components/gsap/src/uncompressed/plugins/CSSRulePlugin',
-     EasePack:
-     '/app/bower_components/gsap/src/uncompressed/easing/EasePack',
-     modernizr:
-     '/app/bower_components/modernizr/modernizr'
-   }
+  paths: {
+   TweenLite:
+   '/app/bower_components/gsap/src/uncompressed/TweenLite',
+   CSSPlugin:
+   '/app/bower_components/gsap/src/uncompressed/plugins/CSSPlugin',
+   CSSRulePlugin:
+   '/app/bower_components/gsap/src/uncompressed/plugins/CSSRulePlugin',
+   EasePack:
+   '/app/bower_components/gsap/src/uncompressed/easing/EasePack',
+   modernizr:
+   '/app/bower_components/modernizr/modernizr'
+  }
 });
 
 require(['modernizr']);
